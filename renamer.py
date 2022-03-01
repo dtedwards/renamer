9335897654bbc54d243cd748beb2778e38f730f2
Dylan Edwards <dtedwards86@gmail.com>
2022-02-28 21:29:26 -0600

    initial commit

---
+++ renamer.py
@@ -0,0 +1,50 @@
+import os
+from os import walk
+import zipfile
+
+#TO DO - add prompt asking for initial zip file name
+#TO DO - add unzip for the initial zip file
+
+#assignments folder
+folder = "../Week6HW/"
+
+def getFiles(folder):
+    #Get all files
+    f = []
+    for (dirpath, dirnames, filenames) in walk(folder):
+        f.extend(filenames) #add the LIST of filenames to f
+        break
+    return f
+
+f = getFiles(folder)
+
+#for each file
+for e in f:
+    #delete index.html
+    if e == "index.html":
+        os.remove(folder + e)
+
+    oldFilename = folder + e
+
+    #if filename has multiple parts
+    if len(e.split("- ")) > 1:
+        #split filename down to student name
+        name = e.split("- ") #get student name
+        name = name[1].split(" ")
+        type = e.split(".") #get file type
+        #construct new filename
+        newFilename = folder + name[0] + name[1] + "." + type[1]
+
+        #save file with new name
+        os.rename(oldFilename, newFilename)
+
+#get new list of files
+f = getFiles(folder)
+#if ends in .zip, then unzip, else do nothing
+for e in f:
+    if e.split(".")[1] == "zip":
+        os.mkdir(folder + e.split(".")[0])
+        #unzip
+        with zipfile.ZipFile(folder + e, "r") as zf:
+            zf.extractall(folder + e.split(".")[0])
+        os.remove(folder + e)
